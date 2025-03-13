56faa42a2f16ea8cdd9ef3c87cfdbfdb63dbee97
Max H <49844637+orangemaxx@users.noreply.github.com>
2025-03-14 12:43:21 +1300

    Flask will now generate static webpages to the build/ folder

--- app.py
+++ app.py
@@ -1,10 +1,27 @@
 from flask import Flask, render_template, url_for
+import os
+import json
+
+with open("pages.json", "r") as file:
+    pages = json.load(file)
 
 
 app = Flask(__name__)
 
-@app.route("/")
-def index():
-    return render_template("index.html", name="Relevent Implications")
+# @app.route("/")
+# def index():
+#     return render_template("index.html", name="Relevent Implications")
+
+
+def savefile(filename, args):
+    current_dir = os.path.dirname(__file__)
+    rel_path = "/build/" + filename
+    with open(current_dir + rel_path, "w") as f:
+        f.write(render_template(filename, **args))
+
+with app.app_context():
+    for page in pages:
+        args = pages[page]
+        savefile(page, args)
 
 app.run(debug=True)
