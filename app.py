3f5e84b27e1fb659bd9753d15891a6f67a1c44f8
Max H <49844637+orangemaxx@users.noreply.github.com>
2025-03-20 10:47:34 +1300

    new page

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ def savefile(filename, args):
     current_dir = os.path.dirname(__file__)
     rel_path = "/build/" + filename
     with open(current_dir + rel_path, "w") as f:
-        f.write(render_template(filename, **args))
+        f.write(render_template("pages/"+filename, **args))
 
 with app.app_context():
     for page in pages:
